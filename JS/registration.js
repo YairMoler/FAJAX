f0c63bad0ec8936881123755c9dab2e2aa256d4b
YairMoller <yairmoller@hilma.tech>
2024-10-13 19:05:43 +0300

    fixed SPA issues

--- JS/registration.js
+++ JS/registration.js
@@ -1,23 +1,21 @@
-let registerValues = {};
-
 document.getElementById("new-username").addEventListener("change", (event) => handleChangeRegister(event));
 
 document.getElementById("new-password").addEventListener("change", (event) => handleChangeRegister(event));
 
 document.getElementById("confirm-password").addEventListener("change", (event) => handleChangeRegister(event));
 
-const handleChangeRegister = (event) => {
+function handleChangeRegister(event) {
     event.preventDefault();
     registerValues[event.target.id] = event.target.value;
     console.log(event.target.value);
-};
+}
 
-const tempFAJAX1 = () => {
+function tempFAJAX1() {
     changePage("login-page");
-};
+}
 
 // to-do: try regex
-const register = (event) => {
+function register(event) {
     event.preventDefault();
     if (registerValues["new-password"] === registerValues["confirm-password"]) {
         if (registerValues["new-password"].length >= 3 && registerValues["confirm-password"].length >= 3) {
@@ -28,4 +26,4 @@ const register = (event) => {
     } else {
         alert("password is not confirmed");
     }
-};
+}
