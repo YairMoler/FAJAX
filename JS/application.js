4431845a48581ae94f180618c2f2052b70f908b8
RamAvni <ramavni@hilma.tech>
2024-10-13 18:19:37 +0300

    Ram's Code > Yair's Code

--- JS/application.js
+++ JS/application.js
@@ -14,13 +14,17 @@ localStorage.setItem(
     }])
 );
 
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+}
+
 function showRecipe(recipeArrStr) {
     let newHTML = ``;
     const recipeArr = JSON.parse(recipeArrStr);
     for (let recipeObj of recipeArr) {
         for (let key in recipeObj) {
             if (key != "steps") {
-                newHTML += `<h2>${key}: ${recipeObj[key]}<h2>`;
+                newHTML += `<h2>${capitalize(key)}: ${recipeObj[key]}<h2>`;
             } else {
                 for (let step of recipeObj["steps"]) newHTML += `<p>${step}</p>`;
             }
