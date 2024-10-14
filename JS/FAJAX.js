24e38c125a60c61fc835eb88a526442edfe5fbef
RamAvni <ramavni@hilma.tech>
2024-10-14 19:08:09 +0300

    eded

--- JS/FAJAX.js
+++ JS/FAJAX.js
@@ -14,8 +14,8 @@ class FAJAX {
 }
 
 let request = new FAJAX();
-request.open("get", "duck/API/recipes/1");
+request.open("post", "duck/API/recipes");
 request.onload = () => {
     console.log(request);
 };
-request.send();
+request.send('{name: "Yair", type: "Dessert", time: "-1H", steps: ["מחבר שלוש מילים" ,"מחמם על אש קטנה" ,"ורץ מהר להביא","בצל מהשכנה" ,"מוסיף שני חרוזים" ,"קצת פלפל, קצת מלח" ,"מערבב שלושה כבשים" ,"וזורק קובייה של קרח"]}');
